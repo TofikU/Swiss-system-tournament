8e0e9d99f363164fabc1ece65dba4357a03aa191
Tofik Umukhanov <umukhanovtofik@gmail.com>
2017-02-23 23:59:52 -0500

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -1,12 +1,3 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
-
-
 -- Deleting databse if already exists
 DROP DATABASE IF EXISTS tournamnet;
 
@@ -34,13 +25,12 @@ CREATE TABLE matches (id SERIAL primary key,
 -- Creating VIEW standings which will present four columns
 -- player id and name, win count, total matches played, 
 -- and displaying players in DESC order by number of wins.
-CREATE OR REPLACE VIEW standings AS
-					SELECT players.id, players.name,
-					SUM(CASE WHEN players.id = matches.winner THEN 1 ELSE 0 END)
-					AS wins, SUM(CASE WHEN players.id = matches.winner
-					OR players.id = matches.loser THEN 1 ELSE 0 END)
-					AS match_count FROM players LEFT JOIN matches ON
-					players.id = matches.winner OR players.id = matches.loser
-					GROUP BY players.id
-					ORDER BY wins DESC,
-					match_count ASC;
+CREATE OR REPLACE VIEW standings AS SELECT players.id, players.name,
+				 SUM(CASE WHEN players.id = matches.winner THEN 1 ELSE 0 END)
+				 AS wins, SUM(CASE WHEN players.id = matches.winner
+					      OR players.id = matches.loser THEN 1 ELSE 0 END)
+				 AS match_count FROM players LEFT JOIN matches
+				 	      ON players.id = matches.winner OR players.id = matches.loser
+				 GROUP BY players.id
+				 ORDER BY wins DESC,
+				 match_count ASC;
